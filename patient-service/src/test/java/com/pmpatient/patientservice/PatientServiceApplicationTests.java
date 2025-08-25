7961ea77a0e609835d78e5e37a05601702573979
szytambsky <szytamborski@gmail.com>
2025-08-25 20:21:42 +0200

    [1][#1] Init commit, add patient-service module

---
+++ patient-service/src/test/java/com/pmpatient/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmpatient.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
