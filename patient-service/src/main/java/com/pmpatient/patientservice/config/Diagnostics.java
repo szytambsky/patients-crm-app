0d6046be22f2d66d02da65065123ea8e6e7da231
szytambsky <szytamborski@gmail.com>
2025-09-11 11:02:59 +0200

    add Diagnostics

---
+++ patient-service/src/main/java/com/pmpatient/patientservice/config/Diagnostics.java
@@ -0,0 +1,28 @@
+package com.pmpatient.patientservice.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class Diagnostics {
+
+    @Bean
+    @Profile("dev")
+    public CommandLineRunner run() {
+        return (args) -> System.out.println("Hello from dev");
+    }
+
+    @Bean
+    @Profile("staging")
+    public CommandLineRunner runStaging() {
+        return (args) -> System.out.println("Hello from staging");
+    }
+
+    @Bean
+    @Profile("prod")
+    public CommandLineRunner runProduction() {
+        return (args) -> System.out.println("Hello from production");
+    }
+}
