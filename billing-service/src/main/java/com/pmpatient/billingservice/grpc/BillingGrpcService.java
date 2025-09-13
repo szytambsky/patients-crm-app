7fbc7629702cdc70e722c8b456606a7fd94eafd9
szytambsky <szytamborski@gmail.com>
2025-09-13 15:08:42 +0200

    setup grpc billing service with proto file and http request test

---
+++ billing-service/src/main/java/com/pmpatient/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,27 @@
+package com.pmpatient.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received: {}", billingRequest.toString());
+
+        // Business logic - e.g. save to database, perform calculations todo;
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
