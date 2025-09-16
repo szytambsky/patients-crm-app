db905feaff9a238d233309b78dff1ec6106add16
Szymon Tamborski <75170932+szytambsky@users.noreply.github.com>
2025-09-16 21:31:13 +0200

    [feature] integrate kafka (#4)
    * update README.md for kafka
    * add kafka producer within patient topic on top of patient-service
    * add analytics-service, as its customer for PatientEvent

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
 # Patient Service – local setup
 
-## Start
+## 🔌 Kafka connection
+
+👉 **Dev (local)**  
+Use in case connecting from host (`localhost`) by port **9094**:
+
+
+👉 **Stage / Prod (docker network)**  
+Services are communicating inside internal pre-created 🐳 Docker network by hostname **kafka** and port **9092**:
+
+## Services Start
+
+create network external network (one-time use only)
+
+TODO: by script ansible/terraform/init.sh
+
+```bash
+docker network create --driver bridge patient-network
+````
 
 ### dev
 ```bash
 docker compose -f ./docker-compose.dev.yml up -d
 ```
+```bash
+docker compose -f ./docker-compose.dev.yml -f infra/docker-compose.kafka.yml up -d
+```
 ### prod
 ```bash
 docker compose -f ./docker-compose.prod.yml up -d
+```
+```bash
+docker compose -f ./docker-compose.prod.yml -f infra/docker-compose.kafka.yml up -d
 ```
 
 ## Stop
---
+++ analytics-service/src/main/java/com/pmpatient/analyticsservice/infrastracture/kafka/KafkaConsumer.java
@@ -0,0 +1,28 @@
+package com.pmpatient.analyticsservice.infrastracture.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer { //todo: kafka consumer config
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            // ... perform any business cases related to analytics
+            log.info("Received Patient Event: [PatientId={}], [PatientName={}], [PatientEmail={}]",
+                    patientEvent.getPatientId(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event: {} ", e.getMessage());
+        }
+    }
+}
