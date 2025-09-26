11d2b1556f09238dd57a80c0f750d88f02644ac5
szytambsky <szytamborski@gmail.com>
2025-09-26 11:48:43 +0200

    use AWS Cdk to create AWS VPC that creates routing and network for internal services

--- infrastracture/src/main/java/com/pmpatient/stack/LocalStack.java
+++ infrastracture/src/main/java/com/pmpatient/stack/LocalStack.java
@@ -5,13 +5,24 @@ import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
     private static final String LOCAL_STACK_NAME = "medtechcrm";
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
     }
 
     public static void main(final String[] args) {
