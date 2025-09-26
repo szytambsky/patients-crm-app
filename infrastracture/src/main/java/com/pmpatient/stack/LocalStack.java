d67334ca29944ba0ccd011126c0673137f07a096
szytambsky <szytamborski@gmail.com>
2025-09-26 12:46:30 +0200

    create 2 AWS RDS instances inside VPC

--- infrastracture/src/main/java/com/pmpatient/stack/LocalStack.java
+++ infrastracture/src/main/java/com/pmpatient/stack/LocalStack.java
@@ -3,9 +3,18 @@ package com.pmpatient.stack;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.Credentials;
+import software.amazon.awscdk.services.rds.DatabaseInstance;
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
+import software.amazon.awscdk.services.rds.PostgresEngineVersion;
+import software.amazon.awscdk.services.rds.PostgresInstanceEngineProps;
 
 public class LocalStack extends Stack {
 
@@ -16,12 +25,32 @@ public class LocalStack extends Stack {
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
         this.vpc = createVpc();
+        DatabaseInstance authServiceDB
+                = createDatabase("AuthServiceDB", "auth-service-db");
+        DatabaseInstance patientServiceDB
+                = createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     private Vpc createVpc() {
         return Vpc.Builder.create(this, "PatientManagementVPC")
                 .vpcName("PatientManagementVPC")
                 .maxAzs(2)
+                .build();
+    }
+
+    private DatabaseInstance createDatabase(String id, String dbName) {
+        return DatabaseInstance.Builder.create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                            .version(PostgresEngineVersion.VER_17_6)
+                            .build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2,
+                        InstanceSize.MICRO))
+                .allocatedStorage(12)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
     }
 
