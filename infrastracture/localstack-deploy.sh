0f2f395a0fce4dbee8ced473913fbc659e53fd9b
szytambsky <szytamborski@gmail.com>
2025-09-30 00:08:51 +0200

    add localstack-deployment script

--- infrastracture/localstack-deploy.sh
+++ infrastracture/localstack-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+
 set -e # Stops the script if any command fails
 
-aws secretsmanager create-secret \
-  --name jwtSecret \
-  --secret-string 'JWT_SECRET from env' \
-  --endpoint-url http://localhost:4566
+aws --endpoint-url=http://localhost:4566 cloudformation deploy \
+    --stack-name patient-management \
+    --template-file "./cdk.out/localstack.template.json"
 
-aws secretsmanager list-secrets --endpoint-url http://localhost:4566
-
+aws --endpoint-url=http://localhost:4566 elbv2 describe-load-balancers \
+    --query "LoadBalancers[0].DNSName" --output text
 
